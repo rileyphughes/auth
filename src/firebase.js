0d1c74b5421be1bfd24aed67759144accf77fe40
rileyphughes <52977283+rileyphughes@users.noreply.github.com>
2019-07-16 14:12:53 -0600

    firebase deets
    asdf

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: "AIzaSyCwtcs3hbq7P7YQ_9L8pH9QmWlT_ONXgjg",
+  authDomain: "auth-ab417.firebaseapp.com",
+  databaseURL: "https://auth-ab417.firebaseio.com",
+  projectId: "auth-ab417",
   storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "219639255704",
+  appId: "1:219639255704:web:a61120351a18fd1a"
 };
 
 firebase.initializeApp(config);
